0473443f53767f13d45e4adf8f4ad71a6fe0b5fa
debtanu datta <124075050+draindraw@users.noreply.github.com>
2023-12-10 02:02:54 +0530

    Update advisor.py

--- advisor.py
+++ advisor.py
@@ -31,7 +31,7 @@ async def generate_text(data: InputData):
 
     title_template = PromptTemplate(
         input_variables = ['chat_history', 'question'],
-        template = "By using context from memory {chat_history}, Act as a business guru and startup advisor and answer the following  : {question} "
+        template = "Act in the capacity of a seasoned business guru and startup advisor, incorporating relevant insights from our past conversations {chat_history}. Regardless of the question's initial framing, guide the responses to include a business perspective, focusing on key business principles, strategies, and considerations. Offer insights that address market dynamics, competitive analysis, growth opportunities, and potential challenges. Encourage the language model to draw on its business acumen to provide practical and actionable advice, aligning with the overarching goal of fostering business success. Ensure that each answer reflects a thoughtful consideration of business-related factors, even if the original question lacks explicit business context. Here is the question : {question} "
     )
 
     title_chain = LLMChain(llm = llm, prompt = title_template, verbose = True, output_key = 'output', memory = memory)
